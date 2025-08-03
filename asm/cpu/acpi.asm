9fcc926422e6483b031d11750db589af99a02893
Federico Ramos <federico.g.ramos@gmail.com>
2025-08-03 04:39:48 -0300

    formatting

--- asm/cpu/acpi.asm
+++ asm/cpu/acpi.asm
@@ -11,22 +11,25 @@
 bits 64
 
 init_acpi:
-	mov al, [p_BootMode]		; Check how the system was booted
-	cmp al, 'U'			; UEFI?
-	je foundACPIfromUEFI		; If so, jump - otherwise fall thru for BIOS
+	mov al, [p_BootMode]	;; How the system was booted.
+	cmp al, 'U'				;; UEFI.
+	je foundACPIfromUEFI
 
-; Find the ACPI RSDP Structure on a BIOS system
-	mov esi, 0x000E0000		; Start looking for the Root System Description Pointer Structure
-	mov rbx, "RSD PTR "		; This in the Signature for the ACPI Structure Table (0x2052545020445352)
+	;; Find the ACPI RSDP Structure on a BIOS system.
+	mov esi, 0x000E0000		;; Start looking for the Root System Description Poi
+							;; nter Structure
+	mov rbx, "RSD PTR "		;; This in the Signature for the ACPI Structure Tabl
+							;; e (0x2052545020445352)
 searchingforACPI:
-	lodsq				; Load a quad word from RSI and store in RAX, then increment RSI by 8
-	cmp rax, rbx			; Verify the Signature
+	lodsq					;; Load a quad word from RSI and store in RAX, then 
+							;; increment RSI by 8.
+	cmp rax, rbx			;; Verify Signature.
 	je foundACPI
-	cmp esi, 0x000FFFFF		; Keep looking until we get here
-	jae acpiFail			; ACPI tables couldn't be found, fail
+	cmp esi, 0x000FFFFF		;; Keep looking until we get here.
+	jae acpiFail			;; ACPI tables couldn't be found, fail.
 	jmp searchingforACPI
 
-; Find the ACPI RSDP Structure on a UEFI system
+;; Find the ACPI RSDP Structure on a UEFI system.
 foundACPIfromUEFI:
 	mov rsi, [0x400000 + 4 * 1024 + 8 * 6]	;; TODO: simbolizar. 0x400000 = base
 											;; addr donde imagen efi se carga. E
@@ -35,7 +38,7 @@ foundACPIfromUEFI:
 	mov rbx, "RSD PTR "	;; Root Sys Description Pointer Table (RSDT). Signature.
 	lodsq				;; Carga signature. Luego de carga, apunta a checksum.
 	cmp rax, rbx
-	jne acpiFail			;; If it isn't a match then fail
+	jne acpiFail		;; If it isn't a match then fail.
 
 ;; Parse the Root System Description Pointer (RSDP) Structure (5.2.5.3)
 foundACPI:			;; Found a Pointer Structure, verify the checksum
@@ -54,84 +57,87 @@ foundACPI:			;; Found a Pointer Structure, verify the checksum
 	
 	pop rsi			;; rsi = RSDP[checksum]
 
-	lodsb			;; Checksum
-	lodsd			;; OEMID (First 4 bytes)
-	lodsw			;; OEMID (Last 2 bytes)
+	lodsb			;; Checksum.
+	lodsd			;; OEMID (First 4 bytes).
+	lodsw			;; OEMID (Last 2 bytes).
 	lodsb			;; Revision (0 is v1.0, 1 is v2.0, 2 is v3.0, etc)
 	cmp al, 0
 	je foundACPIv1	;; If AL is 0 then the system is using ACPI v1.0
-	jmp foundACPIv2	;; Otherwise it is v2.0 or higher
+	jmp foundACPIv2	;; Otherwise it is v2.0 or higher.
 
 ;; TODO: el foundACPIvN se puede juntar en 1 sola funcion o macro que contemple
 ;; las pocas diferencias que hay que considerar.
-foundACPIv1:				; Root System Description Table (RSDT)
+foundACPIv1:		;; Root System Description Table (RSDT)
 	xor eax, eax
-	lodsd				; RsdtAddress - 32 bit physical address of the RSDT (Offset 16)
-	mov rsi, rax			; RSI now points to the RSDT
-	lodsd				; Load Signature
-	cmp eax, 'RSDT'			; Make sure the signature is valid
-	jne novalidacpi			; Not the same? Bail out
+	lodsd			;; RsdtAddress - 32 bit physical address of the RSDT (Offset 16)
+	mov rsi, rax	;; RSI now points to the RSDT.
+	lodsd			;; Load Signature.
+	cmp eax, 'RSDT'
+	jne novalidacpi	;; Not the same, out.
 	sub rsi, 4
-	mov [p_ACPITableAddress], rsi	; Save the RSDT Table Address
+	mov [p_ACPITableAddress], rsi	;; Save RSDT Table Address.
 	add rsi, 4
 	xor eax, eax
-	lodsd				; Length
-	add rsi, 28			; Skip to the Entry offset
-	sub eax, 36			; EAX holds the table size. Subtract the preamble
-	shr eax, 2			; Divide by 4
-	mov rdx, rax			; RDX is the entry count
+	lodsd			;; Length
+	add rsi, 28		;; Skip to the Entry offset
+	sub eax, 36		;; EAX holds the table size. Subtract the preamble
+	shr eax, 2		;; Divide by 4
+	mov rdx, rax	;; RDX is the entry count
 	xor ecx, ecx
+
 foundACPIv1_nextentry:
-	lodsd				; Load a 32-bit Entry address
-	push rax			; Push it to the stack as a 64-bit value
+	lodsd			;; Load a 32-bit Entry address
+	push rax		;; Push it to the stack as a 64-bit value
 	add ecx, 1
 	cmp ecx, edx
 	je findACPITables
 	jmp foundACPIv1_nextentry
 
-foundACPIv2:				; Extended System Description Table (XSDT)
-	lodsd				; RsdtAddress - 32 bit physical address of the RSDT (Offset 16)
-	lodsd				; Length
-	lodsq				; XsdtAddress - 64 bit physical address of the XSDT (Offset 24).
-	mov rsi, rax			; RSI now points to the XSDT
-	lodsd				; Load Signature
-	cmp eax, 'XSDT'			; Make sure the signature is valid
-	jne novalidacpi			; Not the same? Bail out
+foundACPIv2:		;; Extended System Description Table (XSDT)
+	lodsd			;; RsdtAddress - 32 bit physical address of the RSDT (Offset 16)
+	lodsd			;; Length
+	lodsq			;; XsdtAddress - 64 bit physical address of the XSDT (Offset 24).
+	mov rsi, rax	;; RSI now points to the XSDT
+	lodsd			;; Load Signature
+	cmp eax, 'XSDT'	;; Make sure the signature is valid
+	jne novalidacpi	;; Not the same? Bail out
 	sub rsi, 4
-	mov [p_ACPITableAddress], rsi	; Save the XSDT Table Address
+	mov [p_ACPITableAddress], rsi	;; Save the XSDT Table Address
 	add rsi, 4
 	xor eax, eax
-	lodsd				; Length
-	add rsi, 28			; Skip to the start of the Entries (offset 36)
-	sub eax, 36			; EAX holds the table size. Subtract the preamble
-	shr eax, 3			; Divide by 8
-	mov rdx, rax			; RDX is the entry count
+	lodsd			;; Length
+	add rsi, 28		;; Skip to the start of the Entries (offset 36)
+	sub eax, 36		;; EAX holds the table size. Subtract the preamble
+	shr eax, 3		;; Divide by 8
+	mov rdx, rax	;; RDX is the entry count
 	xor ecx, ecx
+
 foundACPIv2_nextentry:
-	lodsq				; Load a 64-bit Entry address
-	push rax			; Push it to the stack
+	lodsq			;; Load a 64-bit Entry address
+	push rax		;; Push it to the stack
 	add ecx, 1
 	cmp ecx, edx
 	jne foundACPIv2_nextentry
 
 findACPITables:
 	xor ecx, ecx
+
 nextACPITable:
-	cmp ecx, edx			; Compare current count to entry count
+	cmp ecx, edx			;; Compare current count to entry count
 	je init_smp_acpi_done
-	pop rsi				; Pop an Entry address from the stack
+	pop rsi					;; Pop an Entry address from the stack
 	lodsd
 	add ecx, 1
-	mov ebx, 'APIC'			; Signature for the Multiple APIC Description Table
+	mov ebx, 'APIC'			;; Signature for the Multiple APIC Description Table
 	cmp eax, ebx
 	je foundAPICTable
-	mov ebx, 'HPET'			; Signature for the HPET Description Table
+	mov ebx, 'HPET'			;; Signature for the HPET Description Table
 	cmp eax, ebx
 	je foundHPETTable
-	mov ebx, 'MCFG'			; Signature for the PCIe Enhanced Configuration Mechanism
+	mov ebx, 'MCFG'			;; Signature for the PCIe Enhanced Config Mechanism
 	cmp eax, ebx
 	je foundMCFGTable
-	mov ebx, 'FACP'			; Signature for the Fixed ACPI Description Table
+	mov ebx, 'FACP'			;; Signature for the Fixed ACPI Description Table
 	cmp eax, ebx
 	je foundFADTTable
 	jmp nextACPITable
@@ -157,112 +163,120 @@ init_smp_acpi_done:
 
 acpiFail:
 novalidacpi:
-	; Set screen to Teal
 
-	mov rdi, [0x00005F00]		; Frame buffer base
-	mov rcx, [0x00005F08]		; Frame buffer size
-	shr rcx, 2			; Quick divide by 4
-	mov eax, 0x0000FFFF		; 0x00RRGGBB
+	mov rdi, [0x00005F00]	;; Frame buffer base
+	mov rcx, [0x00005F08]	;; Frame buffer size
+	shr rcx, 2
+	mov eax, 0x0000FFFF		;; 0x00RRGGBB screen to Teal.
 	rep stosd
 	jmp $
 
 
-; -----------------------------------------------------------------------------
-; 5.2.12 Multiple APIC Description Table (MADT)
-; Chapter 5.2.12
+;;==============================================================================
+;; 5.2.12 Multiple APIC Description Table (MADT). Chapter 5.2.12
+;;==============================================================================
+
 parseAPICTable:
 	push rcx
 	push rdx
 
-	lodsd				; Length of MADT in bytes
-	mov ecx, eax			; Store the length in ECX
-	xor ebx, ebx			; EBX is the counter
-	lodsb				; Revision
-	lodsb				; Checksum
-	lodsd				; OEMID (First 4 bytes)
-	lodsw				; OEMID (Last 2 bytes)
-	lodsq				; OEM Table ID
-	lodsd				; OEM Revision
-	lodsd				; Creator ID
-	lodsd				; Creator Revision
-	lodsd				; Local APIC Address (This should match what was pulled already via the MSR)
-	lodsd				; Flags (1 = Dual 8259 Legacy PICs Installed)
+	lodsd			;; Length of MADT in bytes
+	mov ecx, eax	;; Store the length in ECX
+	xor ebx, ebx	;; EBX is the counter
+	lodsb			;; Revision
+	lodsb			;; Checksum
+	lodsd			;; OEMID (First 4 bytes)
+	lodsw			;; OEMID (Last 2 bytes)
+	lodsq			;; OEM Table ID
+	lodsd			;; OEM Revision
+	lodsd			;; Creator ID
+	lodsd			;; Creator Revision
+	lodsd			;; Local APIC Address (This should match what was pulled already via the MSR)
+	lodsd			;; Flags (1 = Dual 8259 Legacy PICs Installed)
 	add ebx, 44
-	mov rdi, 0x0000000000005100	; Valid CPU IDs
+	mov rdi, 0x0000000000005100	;; Valid CPU IDs
 
 readAPICstructures:
 	cmp ebx, ecx
 	jae parseAPICTable_done
-	lodsb				; APIC Structure Type
-	cmp al, 0x00			; Processor Local APIC
+	lodsb						;; APIC Structure Type
+	cmp al, 0x00				;; Processor Local APIC
 	je APICapic
-	cmp al, 0x01			; I/O APIC
+	cmp al, 0x01				;; I/O APIC
 	je APICioapic
-	cmp al, 0x02			; Interrupt Source Override
+	cmp al, 0x02				;; Interrupt Source Override
 	je APICinterruptsourceoverride
-;	cmp al, 0x03			; Non-maskable Interrupt Source (NMI)
-;	je APICnmi
-;	cmp al, 0x04			; Local APIC NMI
-;	je APIClocalapicnmi
-;	cmp al, 0x05			; Local APIC Address Override
-;	je APICaddressoverride
-;	cmp al, 0x06			; I/O SAPIC Structure
-;	je APICiosapic
-;	cmp al, 0x07			; Local SAPIC Structure
-;	je APIClocalsapic
-;	cmp al, 0x08			; Platform Interrupt Source Structure
-;	je APICplatformint
-;	cmp al, 0x09			; Processor Local x2APIC
-;	je APICx2apic
-;	cmp al, 0x0A			; Local x2APIC NMI
-;	je APICx2nmi
+;;	cmp al, 0x03				;; Non-maskable Interrupt Source (NMI)
+;;	je APICnmi
+;;	cmp al, 0x04				;; Local APIC NMI
+;;	je APIClocalapicnmi
+;;	cmp al, 0x05				;; Local APIC Address Override
+;;	je APICaddressoverride
+;;	cmp al, 0x06				;; I/O SAPIC Structure
+;;	je APICiosapic
+;;	cmp al, 0x07				;; Local SAPIC Structure
+;;	je APIClocalsapic
+;;	cmp al, 0x08				;; Platform Interrupt Source Structure
+;;	je APICplatformint
+;;	cmp al, 0x0	9				;; Processor Local x2APIC
+;;	je APICx2apic
+;;	cmp al, 0x0A				;; Local x2APIC NMI
+;;	je APICx2nmi
 
 	jmp APICignore
 
-; Processor Local APIC Structure - 5.2.12.2
-APICapic:				; Entry Type 0
+;;==============================================================================
+;; Processor Local APIC Structure - 5.2.12.2
+;;==============================================================================
+
+APICapic:						; Entry Type 0
 	xor eax, eax
 	xor edx, edx
-	lodsb				; Length (will be set to 8)
+	lodsb						; Length (will be set to 8)
 	add ebx, eax
-	lodsb				; ACPI Processor ID
-	lodsb				; APIC ID
-	xchg eax, edx			; Save the APIC ID to EDX
-	lodsd				; Flags (Bit 0 set if enabled/usable)
-	bt eax, 0			; Test to see if usable
+	lodsb						; ACPI Processor ID
+	lodsb						; APIC ID
+	xchg eax, edx				; Save the APIC ID to EDX
+	lodsd						; Flags (Bit 0 set if enabled/usable)
+	bt eax, 0					; Test to see if usable
 	jnc readAPICstructures		; Read the next structure if CPU not usable
 	inc word [p_cpu_detected]
-	xchg eax, edx			; Restore the APIC ID back to EAX
-	stosb				; Store the 8-bit APIC ID
+	xchg eax, edx				; Restore the APIC ID back to EAX
+	stosb						; Store the 8-bit APIC ID
 	jmp readAPICstructures		; Read the next structure
 
-; I/O APIC Structure - 5.2.12.3
-APICioapic:				; Entry Type 1
+
+;;==============================================================================
+;; I/O APIC Structure - 5.2.12.3
+;;==============================================================================
+
+APICioapic:						; Entry Type 1
 	xor eax, eax
-	lodsb				; Length (will be set to 12)
+	lodsb						; Length (will be set to 12)
 	add ebx, eax
 	push rdi
 	push rcx
 	mov rdi, IM_IOAPICAddress	; Copy this data directly to the InfoMap
 	xor ecx, ecx
 	mov cl, [p_IOAPICCount]
-	shl cx, 4			; Quick multiply by 16
+	shl cx, 4
 	add rdi, rcx
 	pop rcx
 	xor eax, eax
-	lodsb				; IO APIC ID
+	lodsb						; IO APIC ID
 	stosd
-	lodsb				; Reserved
-	lodsd				; I/O APIC Address
+	lodsb						; Reserved
+	lodsd						; I/O APIC Address
 	stosd
-	lodsd				; Global System Interrupt Base
+	lodsd						; Global System Interrupt Base
 	stosd
 	pop rdi
 	inc byte [p_IOAPICCount]
 	jmp readAPICstructures		; Read the next structure
 
+
 ; Interrupt Source Override Structure - 5.2.12.5
-APICinterruptsourceoverride:		; Entry Type 2
+APICinterruptsourceoverride:	; Entry Type 2
 	xor eax, eax
 	lodsb				; Length (will be set to 10)
 	add ebx, eax
