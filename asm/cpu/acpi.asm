4593697b6283ebed7233f19dc8b24079e3fca897
Federico Ramos <federico.g.ramos@gmail.com>
2025-08-03 05:58:07 -0300

    tidying code

--- asm/cpu/acpi.asm
+++ asm/cpu/acpi.asm
@@ -93,23 +93,23 @@ foundACPIv1_nextentry:
 	je findACPITables
 	jmp foundACPIv1_nextentry
 
-foundACPIv2:		;; Extended System Description Table (XSDT)
-	lodsd			;; RsdtAddress - 32 bit physical address of the RSDT (Offset 16)
-	lodsd			;; Length
-	lodsq			;; XsdtAddress - 64 bit physical address of the XSDT (Offset 24).
-	mov rsi, rax	;; RSI now points to the XSDT
-	lodsd			;; Load Signature
-	cmp eax, 'XSDT'	;; Make sure the signature is valid
-	jne novalidacpi	;; Not the same? Bail out
+foundACPIv2:		;; Extended System Description Table (XSDT).
+	lodsd			;; RsdtAddress - 32 bit physical addr of RSDT (Offset 16).
+	lodsd			;; Length.
+	lodsq			;; XsdtAddress - 64 bit physical addr of XSDT (Offset 24).
+	mov rsi, rax	;; RSI now points to the XSDT.
+	lodsd			;; Load Signature.
+	cmp eax, 'XSDT'	;; Make sure the signature is valid.
+	jne novalidacpi	;; Not the same? Bail out.
 	sub rsi, 4
-	mov [p_ACPITableAddress], rsi	;; Save the XSDT Table Address
+	mov [p_ACPITableAddress], rsi	;; Save the XSDT Table Address.
 	add rsi, 4
 	xor eax, eax
-	lodsd			;; Length
-	add rsi, 28		;; Skip to the start of the Entries (offset 36)
-	sub eax, 36		;; EAX holds the table size. Subtract the preamble
-	shr eax, 3		;; Divide by 8
-	mov rdx, rax	;; RDX is the entry count
+	lodsd			;; Length.
+	add rsi, 28		;; Skip to the start of the Entries (offset 36).
+	sub eax, 36		;; EAX holds the table size. Subtract the preamble.
+	shr eax, 3
+	mov rdx, rax	;; RDX is the entry count.
 	xor ecx, ecx
 
 foundACPIv2_nextentry:
@@ -225,145 +225,155 @@ readAPICstructures:
 
 	jmp APICignore
 
+
 ;;==============================================================================
 ;; Processor Local APIC Structure - 5.2.12.2
 ;;==============================================================================
 
-APICapic:						; Entry Type 0
+APICapic:						;; Entry Type 0
 	xor eax, eax
 	xor edx, edx
-	lodsb						; Length (will be set to 8)
+	lodsb						;; Length (will be set to 8)
 	add ebx, eax
-	lodsb						; ACPI Processor ID
-	lodsb						; APIC ID
-	xchg eax, edx				; Save the APIC ID to EDX
-	lodsd						; Flags (Bit 0 set if enabled/usable)
-	bt eax, 0					; Test to see if usable
-	jnc readAPICstructures		; Read the next structure if CPU not usable
+	lodsb						;; ACPI Processor ID
+	lodsb						;; APIC ID
+	xchg eax, edx				;; Save the APIC ID to EDX
+	lodsd						;; Flags (Bit 0 set if enabled/usable)
+	bt eax, 0					;; Test to see if usable
+	jnc readAPICstructures		;; Read the next structure if CPU not usable
 	inc word [p_cpu_detected]
-	xchg eax, edx				; Restore the APIC ID back to EAX
-	stosb						; Store the 8-bit APIC ID
-	jmp readAPICstructures		; Read the next structure
+	xchg eax, edx				;; Restore the APIC ID back to EAX
+	stosb						;; Store the 8-bit APIC ID
+	jmp readAPICstructures		;; Read the next structure
 
 
 ;;==============================================================================
 ;; I/O APIC Structure - 5.2.12.3
 ;;==============================================================================
 
-APICioapic:						; Entry Type 1
+APICioapic:						;; Entry Type 1
 	xor eax, eax
-	lodsb						; Length (will be set to 12)
+	lodsb						;; Length (will be set to 12)
 	add ebx, eax
 	push rdi
 	push rcx
-	mov rdi, IM_IOAPICAddress	; Copy this data directly to the InfoMap
+	mov rdi, IM_IOAPICAddress	;; Copy this data directly to the InfoMap
 	xor ecx, ecx
 	mov cl, [p_IOAPICCount]
 	shl cx, 4
 	add rdi, rcx
 	pop rcx
 	xor eax, eax
-	lodsb						; IO APIC ID
+	lodsb						;; IO APIC ID
 	stosd
-	lodsb						; Reserved
-	lodsd						; I/O APIC Address
+	lodsb						;; Reserved
+	lodsd						;; I/O APIC Address
 	stosd
-	lodsd						; Global System Interrupt Base
+	lodsd						;; Global System Interrupt Base
 	stosd
 	pop rdi
 	inc byte [p_IOAPICCount]
-	jmp readAPICstructures		; Read the next structure
+	jmp readAPICstructures		;; Read the next structure
 
 
-; Interrupt Source Override Structure - 5.2.12.5
-APICinterruptsourceoverride:	; Entry Type 2
+;;==============================================================================
+;; Interrupt Source Override Structure - 5.2.12.5
+;;==============================================================================
+
+APICinterruptsourceoverride:	;; Entry Type 2
 	xor eax, eax
-	lodsb				; Length (will be set to 10)
+	lodsb						;; Length (will be set to 10)
 	add ebx, eax
 	push rdi
 	push rcx
-	mov rdi, IM_IOAPICIntSource	; Copy this data directly to the InfoMap
+	mov rdi, IM_IOAPICIntSource	;; Copy this data directly to the InfoMap
 	xor ecx, ecx
 	mov cl, [p_IOAPICIntSourceC]
-	shl cx, 3			; Quick multiply by 8
+	shl cx, 3
 	add rdi, rcx
-	lodsb				; Bus Source
+	lodsb				;; Bus Source
 	stosb
-	lodsb				; IRQ Source
+	lodsb				;; IRQ Source
 	stosb
-	lodsd				; Global System Interrupt
+	lodsd				;; Global System Interrupt
 	stosd
-	lodsw				; Flags - bit 1 Low(1)/High(0), Bit 3 Level(1)/Edge(0)
+	lodsw				;; Flags - bit 1 Low(1)/High(0), Bit 3 Level(1)/Edge(0)
 	stosw
 	pop rcx
 	pop rdi
 	inc byte [p_IOAPICIntSourceC]
-	jmp readAPICstructures		; Read the next structure
+	jmp readAPICstructures	;; Read the next structure
 
-; Processor Local x2APIC Structure - 5.2.12.12
-;APICx2apic:				; Entry Type 9
-;	xor eax, eax
-;	xor edx, edx
-;	lodsb				; Length (will be set to 16)
-;	add ebx, eax
-;	lodsw				; Reserved; Must be Zero
-;	lodsd
-;	xchg eax, edx			; Save the x2APIC ID to EDX
-;	lodsd				; Flags (Bit 0 set if enabled/usable)
-;	bt eax, 0			; Test to see if usable
-;	jnc APICx2apicEnd		; Read the next structure if CPU not usable
-;	xchg eax, edx			; Restore the x2APIC ID back to EAX
-;	; TODO - Save the ID's somewhere
-;APICx2apicEnd:
-;	lodsd				; ACPI Processor UID
-;	jmp readAPICstructures		; Read the next structure
+;; Processor Local x2APIC Structure - 5.2.12.12
+;;APICx2apic:			;; Entry Type 9
+;;	xor eax, eax
+;;	xor edx, edx
+;;	lodsb				;; Length (will be set to 16)
+;;	add ebx, eax
+;;	lodsw				;; Reserved; Must be Zero
+;;	lodsd
+;;	xchg eax, edx		;; Save the x2APIC ID to EDX
+;;	lodsd				;; Flags (Bit 0 set if enabled/usable)
+;;	bt eax, 0			;; Test to see if usable
+;;	jnc APICx2apicEnd	;; Read the next structure if CPU not usable
+;;	xchg eax, edx		;; Restore the x2APIC ID back to EAX
+;;;;;;;;;;;;;;;;;;;;;;;;;; TODO - Save the ID's somewhere
+;;APICx2apicEnd:
+;;	lodsd					;; ACPI Processor UID
+;;	jmp readAPICstructures	;; Read the next structure
 
 APICignore:
 	xor eax, eax
-	lodsb				; We have a type that we ignore, read the next byte
+	lodsb					;; We have a type that we ignore, read the next byte
 	add ebx, eax
 	add rsi, rax
-	sub rsi, 2			; For the two bytes just read
-	jmp readAPICstructures		; Read the next structure
+	sub rsi, 2				;; For the two bytes just read
+	jmp readAPICstructures	;; Read the next structure.
 
 parseAPICTable_done:
 	pop rdx
 	pop rcx
 	ret
-; -----------------------------------------------------------------------------
 
+;;==============================================================================
+;; High Precision Event Timer (HPET)
+;;==============================================================================
+;; http://www.intel.com/content/dam/www/public/us/en/documents/technical-specifi
+;; cations/software-developers-hpet-spec-1-0a.pdf
+;;==============================================================================
 
-; -----------------------------------------------------------------------------
-; High Precision Event Timer (HPET)
-; http://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/software-developers-hpet-spec-1-0a.pdf
 parseHPETTable:
-	lodsd				; Length of HPET in bytes
-	lodsb				; Revision
-	lodsb				; Checksum
-	lodsd				; OEMID (First 4 bytes)
-	lodsw				; OEMID (Last 2 bytes)
-	lodsq				; OEM Table ID
-	lodsd				; OEM Revision
-	lodsd				; Creator ID
-	lodsd				; Creator Revision
+	lodsd						;; Length of HPET in bytes.
+	lodsb						;; Revision.
+	lodsb						;; Checksum.
+	lodsd						;; OEMID (First 4 bytes).
+	lodsw						;; OEMID (Last 2 bytes).
+	lodsq						;; OEM Table ID.
+	lodsd						;; OEM Revision.
+	lodsd						;; Creator ID.
+	lodsd						;; Creator Revision.
 
-	lodsb				; Hardware Revision ID
-	lodsb				; # of Comparators (5:0), COUNT_SIZE_CAP (6), Legacy IRQ (7)
-	lodsw				; PCI Vendor ID
-	lodsd				; Generic Address Structure
-	lodsq				; Base Address Value
-	mov [p_HPET_Address], rax	; Save the Address of the HPET
-	lodsb				; HPET Number
-	lodsw				; Main Counter Minimum
-	mov [p_HPET_CounterMin], ax	; Save the Counter Minimum
-	lodsb				; Page Protection And OEM Attribute
+	lodsb						;; Hardware Revision ID.
+	lodsb						;; # of Comparators (5:0), COUNT_SIZE_CAP (6), L
+								;; egacy IRQ (7).
+	lodsw						;; PCI Vendor ID.
+	lodsd						;; Generic Address Structure.
+	lodsq						;; Base Address Value.
+	mov [p_HPET_Address], rax	;; Save the Address of the HPET.
+	lodsb						;; HPET Number.
+	lodsw						;; Main Counter Minimum.
+	mov [p_HPET_CounterMin], ax	;; Save the Counter Minimum.
+	lodsb						;; Page Protection And OEM Attribute.
 	ret
-; -----------------------------------------------------------------------------
 
 
-; -----------------------------------------------------------------------------
-; PCI Express Memory-mapped Configuration (MCFG)
-; Locked behind a paywall - Search Google for "pcie specification pdf"
+;;==============================================================================
+;; PCI Express Memory-mapped Configuration (MCFG)
+;;==============================================================================
+;; pcie specification:
+;; https://picture.iczhiku.com/resource/eetop/SYkDTqhOLhpUTnMx.pdf
+;;==============================================================================
+
 parseMCFGTable:
 	push rdi
 	push rcx
@@ -373,77 +383,75 @@ parseMCFGTable:
 	shl ecx, 4
 	mov rdi, IM_PCIE
 	add rdi, rcx
-	lodsd				; Length of MCFG in bytes
-	sub eax, 44			; Subtract the size of the table header
-	shr eax, 4			; Quick divide by 16
-	mov ecx, eax			; ECX now stores the number of 16-byte records
+	lodsd						;; Length of MCFG in bytes.
+	sub eax, 44					;; Subtract the size of the table header.
+	shr eax, 4
+	mov ecx, eax				;; ECX now stores the number of 16-byte records.
 	add word [p_PCIECount], cx
-	lodsb				; Revision
-	lodsb				; Checksum
-	lodsd				; OEMID (First 4 bytes)
-	lodsw				; OEMID (Last 2 bytes)
-	lodsq				; OEM Table ID
-	lodsd				; OEM Revision
-	lodsd				; Creator ID
-	lodsd				; Creator Revision
-	lodsq				; Reserved
+	lodsb						;; Revision.
+	lodsb						;; Checksum.
+	lodsd						;; OEMID (First 4 bytes).
+	lodsw						;; OEMID (Last 2 bytes).
+	lodsq						;; OEM Table ID.
+	lodsd						;; OEM Revision.
+	lodsd						;; Creator ID.
+	lodsd						;; Creator Revision.
+	lodsq						;; Reserved.
 
-	; Loop through each entry
+;; Loop through each entry.
 parseMCFGTable_next:
-	lodsq				; Base address of enhanced configuration mechanism
+	lodsq					;; Base address of enhanced configuration mechanism.
 	stosq
-	lodsw				; PCI Segment Group Number
+	lodsw					;; PCI Segment Group Number.
 	stosw
-	lodsb				; Start PCI bus number decoded by this host bridge
+	lodsb					;; Start PCI bus number decoded by this host bridge.
 	stosb
-	lodsb				; End PCI bus number decoded by this host bridge
+	lodsb					;; End PCI bus number decoded by this host bridge.
 	stosb
-	lodsd				; Reserved
+	lodsd					;; Reserved.
 	stosd
 	sub ecx, 1
 	jnz parseMCFGTable_next
 	xor eax, eax
-	not rax				; 0xFFFFFFFFFFFFFFFF
-	stosq				; Mark the end of the table
+	not rax					;; 0xFFFFFFFFFFFFFFFF
+	stosq					;; Mark the end of the table.
 	stosq
 
 	pop rcx
 	pop rdi
 	ret
-; -----------------------------------------------------------------------------
 
 
-; -----------------------------------------------------------------------------
-; Fixed ACPI Description Table (FADT)
-; Chapter 5.2.9
+;;==============================================================================
+;; Fixed ACPI Description Table (FADT). Chapter 5.2.9
+;;==============================================================================
+
+;; At this point RSI points to offset 4 for the FADT.
 parseFADTTable:
-	; At this point RSI points to offset 4 for the FADT
-	sub rsi, 4			; Set RSI back to start to make offsets easier below
 
-	; Gather IAPC_BOOT_ARCH
-	mov eax, [rsi+10]		; Check start of OEMID
-	cmp eax, 0x48434F42		; Is it "BOCH"?
-	je parseFADTTable_end		; If so, bail out
-	mov ax, [rsi+109]		; IAPC_BOOT_ARCH (IA-PC Boot Architecture Flags - 5.2.9.3)
-	mov [p_IAPC_BOOT_ARCH], ax	; Save the IAPC_BOOT_ARCH word
+	sub rsi, 4			;; Set RSI back to start to make offsets easier below
 
-;	add rsi, 116			; RESET_REG (Generic Address Structure - 5.2.3.2)
-;	lodsb				; Address Space ID (0x00 = Memory, 0x01 = I/O, 0x02 = PCI)
-;	lodsb				; Register Width
-;	lodsb				; Register Offset
-;	lodsb				; Access Size
-;	lodsq				; Address
-;	lodsb				; RESET_VALUE
+	;; Gather IAPC_BOOT_ARCH
+	mov eax, [rsi + 10]			;; Check start of OEMID.
+	cmp eax, 0x48434F42			;; Is it "BOCH"?
+	je parseFADTTable_end		;; If so, bail out.
+	mov ax, [rsi + 109]			;; IAPC_BOOT_ARCH (IA-PC Boot Architecture Flags
+								;; - 5.2.9.3).
+	mov [p_IAPC_BOOT_ARCH], ax	;; Save the IAPC_BOOT_ARCH word.
 
-;	add rsi, 36
-;	lodsd				; DSDT
-;	add rsi, 20
-;	lodsd				; PM1a_CNT_BLK
+;;	add rsi, 116	;; RESET_REG (Generic Address Structure - 5.2.3.2).
+;;	lodsb			;; Address Space ID (0x00 = Memory, 0x01 = I/O, 0x02 = PCI).
+;;	lodsb			;; Register Width.
+;;	lodsb			;; Register Offset.
+;;	lodsb			;; Access Size.
+;;	lodsq			;; Address.
+;;	lodsb			;; RESET_VALUE.
+
+;;	add rsi, 36
+;;	lodsd			;; DSDT
+;;	add rsi, 20
+;;	lodsd			;; PM1a_CNT_BLK
 
 parseFADTTable_end:
 	ret
-; -----------------------------------------------------------------------------
 
-
-; =============================================================================
-; EOF
