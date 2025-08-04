4b2fa4fd4b137d3ad7e062c6caa31d314b8d0b55
Federico Ramos <federico.g.ramos@gmail.com>
2025-08-04 03:02:37 -0300

    en este momento ya ha sido probado en maquinas oka

--- asm/cpu/smp.asm
+++ asm/cpu/smp.asm
@@ -7,11 +7,11 @@ init_smp:
 	cmp byte [cfg_smpinit], 1	;; Check if SMP should be enabled
 	jne noMP					;; If not then skip SMP init
 
-	;; Start the AP's one by one
+	;; Start the AP's one by one.
 	xor eax, eax
 	xor edx, edx
 	mov rsi, [p_LocalAPICAddress]
-	mov eax, [rsi+0x20]			;; Add the offset for the APIC ID location
+	mov eax, [rsi + 0x20]			;; Add the offset for the APIC ID location
 	shr rax, 24					;; APIC ID is stored in bits 31:24
 	mov dl, al					;; Store BSP APIC ID in DL
 
@@ -28,7 +28,7 @@ smp_send_INIT:
 	cmp al, dl					;; Is it the BSP?
 	je smp_send_INIT_skipcore
 
-	;; Send 'INIT' IPI to APIC ID in AL
+	;; Send 'INIT' IPI to APIC ID in al.
 	mov rdi, [p_LocalAPICAddress]
 	shl eax, 24
 	mov dword [rdi+0x310], eax	;; Interrupt Command Register (ICR); bits 63-32
@@ -46,7 +46,7 @@ smp_send_INIT_skipcore:
 
 smp_send_INIT_done:
 
-	mov eax, 500			;; Wait 500 microseconds
+	mov eax, 500				;; Wait 500us
 	call os_hpet_delay
 
 	mov esi, IM_DetectedCoreIDs
@@ -58,14 +58,14 @@ smp_send_SIPI:
 	je smp_send_SIPI_done
 	lodsb
 
-	cmp al, dl				;; Is it the BSP?
+	cmp al, dl					;; Is it the BSP?
 	je smp_send_SIPI_skipcore
 
 	;; Send 'Startup' IPI to destination using vector 0x08 to specify entry-point is at the memory-address 0x00008000
 	mov rdi, [p_LocalAPICAddress]
 	shl eax, 24
 	mov dword [rdi+0x310], eax	;; Interrupt Command Register (ICR); bits 63-32
-	mov eax, 0x00004608		; Vector 0x08
+	mov eax, 0x00004608			;; Vector 0x08
 	mov dword [rdi+0x300], eax	;; Interrupt Command Register (ICR); bits 31-0
 
 smp_send_SIPI_verify:
